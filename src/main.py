6a469e408db3a97bcfc5bc22245389bf4355bdd3
felipedestroo <fdestrocontact@gmail.com>
2024-11-28 09:13:04 -0300

    feat: restart/exit button

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
+import pygame
 from game_logic import initialize_game, update_game_state
 from ui import run_start_screen, run_game_loop
 
 
 def main():
+    # Display the start screen and wait for the player's action
     action = run_start_screen()
 
     if action == "play":
         game_state = initialize_game()
-        run_game_loop(game_state, update_game_state)
+        
+        while True:
+            result = run_game_loop(game_state, update_game_state)
 
+            if result == "exit":
+                pygame.quit()  # Exit the game if the player chooses "Exit"
+                break
+            elif result == "restart":
+                game_state = initialize_game()  # Reset the game state if "Restart" is clicked
+                continue  # Start a new game loop
 
 if __name__ == "__main__":
     main()
-
